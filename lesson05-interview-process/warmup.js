063a01422b11f3ba23d685ec924ae7f2f77cf453
Andres Varon <anvaron@gmail.com>
2025-01-28 11:29:10 -0500

    Lesson 05 completed

--- lesson05-interview-process/warmup.js
+++ lesson05-interview-process/warmup.js
@@ -1,9 +1,31 @@
-function reverseString(str) {}
+/**
+ * 
+ * @param {*} str 
+ * @returns
+ * Approach:
+ * convert the string to an array of characters
+ * reverse the array
+ * join the array to form a string
+ */
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
 console.log(reverseString("Hello, World!"));
 // Expected Output: "!dlroW ,olleH"
 
-function capitalizeWords(str) {}
+/**
+ * 
+ * @param {*} str 
+ * @returns 
+ * Approach:
+ * split the string by spaces to form an array of words
+ * map the array of words to capitalize the first letter of each word
+ * join the array of words to form a string
+ */
+function capitalizeWords(str) {
+  return str.split(' ').map(i => i[0].toUpperCase() + i.slice(1)).join(' ');
+}
 
 console.log(capitalizeWords("hello, world!"));
 // Expected Output: 'Hello, World!'
@@ -11,7 +33,17 @@ console.log(capitalizeWords("hello, world!"));
 console.log(capitalizeWords("javascript is awesome"));
 // Expected Output: 'Javascript Is Awesome'
 
-function countVowels(str) {}
+/**
+ * 
+ * @param {*} str 
+ * @returns 
+ * Approach:
+  * match all the vowels in the string
+  * return the length of the matched vowels
+ */
+function countVowels(str) {
+  return str.match(/[aeiou]/gi).length;
+}
 
 console.log(countVowels("hello"));
 // Expected Output: 2
